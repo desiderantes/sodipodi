2be552e0f4b4de694649a21afa5087c72c47db92
lauris <lauris@kaplinski.com>
2023-09-12 19:55:12 +0000

    Added memory check

--- libarikkei/arikkei-utils.c
+++ libarikkei/arikkei-utils.c
@@ -9,11 +9,20 @@
  * This code is in public domain
  */
 
+#include <assert.h>
+#include <crtdbg.h>
+
 #include <malloc.h>
 #include <string.h>
 #include <stdio.h>
 
 #include "arikkei-utils.h"
+
+void
+arikkei_check_integrity (void)
+{
+	assert (_CrtCheckMemory());
+}
 
 unsigned int
 arikkei_emit_fail_warning (const unsigned char *file, unsigned int line, const unsigned char *method, const unsigned char *expr)
